44630556179ef928e00ccd536a97527e8b85a73b
Dinuka Dilshan <73058516+Dinuka-Dilshan@users.noreply.github.com>
2021-06-16 21:31:53 +0530

    added buttons to home page

--- UserInterfaces/HomeRceptionist.cs
+++ UserInterfaces/HomeRceptionist.cs
@@ -14,5 +14,50 @@ namespace ShineWay.UserInterfaces
         {
             InitializeComponent();
         }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
