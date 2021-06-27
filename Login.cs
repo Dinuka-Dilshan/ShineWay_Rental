78d9c9757a87b91152c4cb86b2fc5008c8b42707
Dinuka Dilshan <73058516+Dinuka-Dilshan@users.noreply.github.com>
2021-06-27 16:28:02 +0530

    incorrect password or username login bug fixed

--- Login.cs
+++ Login.cs
@@ -48,22 +48,34 @@ namespace ShineWay
               
                 string query = " SELECT `username`,`user_type` FROM `users`   WHERE username = '" + txt_UserName.Text.Trim() + "' AND password = '" + txtPassword.Text.Trim() + "';";
                 MySqlDataReader reader = DbConnection.readData(query);
-                
+                bool loginStatus = false;
 
-                if (reader == null)
-                {
-                    new CustomMessage("User name or password is Incorrect..!").Show();
-                }
+               
                 while (reader.Read())
                 {
                     if(reader[0].ToString().Equals(txt_UserName.Text.Trim()))
                     {
+                        loginStatus = true;
                         this.Hide();
                         var form2 = new HomeRceptionist(reader[1].ToString());
                         form2.Closed += (s, args) => this.Close();
                         form2.Show();
+                        new CustomMessage("Welcome..!").Show();
+
+                    }
+                    else
+                    {
+                        
+                        break;
                     }
                 }
+
+                if (!loginStatus)
+                {
+                    new CustomMessage("User name or password is Incorrect..!").Show();
+                }
+                
+
             }
 
         }
